ab513d0815031bc1995fea32703d0cf1042a8ed0
Rich Kulesus <richkulesus@gmail.com>
2024-09-05 15:05:07 +0000

    Merged.

---
+++ RK-grout/pngs/convert-png-to-pixmap.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ -x `which convert` ]
+then
+    OGFILE=$1
+    filepath=$OGFILE
+    filename=$(basename "$filepath")
+    file="${filename%.*}"
+    convert "$1" "../pixmaps/$file.xpm"
+else
+    echo "Could not find ImageMagick 'convert'. Aborting."
+fi
